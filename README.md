5736db29c5ea32f2e4e270211a64b4400b8f2ff9
1rom11dev <76579908+1rom11@users.noreply.github.com>
2021-10-14 22:10:22 +0000

    hk

---
+++ README.md
@@ -0,0 +1,16 @@
+# UDBSH
+
+## Unofficial Discord Bot SHell
+
+### A bot that is terminal like. Uses commands like 'echo' , 'sudo'
+
+Made by 1rom11 in replit.
+All code is free to use
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+
+[Check here for more infomation](https://creativecommons.org/licenses/by/4.0/)
+
+And check LICENSE.MD
+
+> 1rom11
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-import discord
-import time
-import discord.ext
-import logging
-from discord.utils import get
-from discord.ext import commands, tasks
-from discord.ext.commands import has_permissions,  CheckFailure, check
+import os, discord, time, random, asyncio, json, logging
+from discord.ext import commands
+from dotenv import load_dotenv
 
 
 logger = logging.getLogger('Discord')
@@ -16,36 +11,84 @@ logger.addHandler(handler)
 
 client = discord.Client()
 
-client = commands.Bot(command_prefix = '$')
+def get_prefix(client, message):
+	with open ('prefix.json' , 'r') as f:
+		prefixes = json.load(f)
+	return prefixes[str(message.guild.id)]
+
+
+client = commands.Bot(command_prefix = get_prefix, help_command=None, case_insensitive=True)
 client.author_id = 487258918465306634
 
 @client.event
 async def on_ready():
-    print(f"{client.user} is online")
+    await client.change_presence(activity=discord.Streaming(name='Terminal', url="https://twitch.tv/1rom111"))
+    
+    print('Connected to bot: {}'.format(client.user.name))
+    print('Bot ID: {}'.format(client.user.id))
 
+@client.event
+async def on_guild_join(guild):
+	with open ('prefix.json' , 'r') as f:
+		prefixes = json.load(f)
+	prefixes[str(guild.id)] = '$'
+
+	with open ('prefix.json', 'w') as f:
+		json.dump(prefixes, f, indent=4)
+
+@client.event
+async def on_guild_remove(guild):
+	with open ('prefix.json' , 'r') as f:
+		prefixes = json.load(f)
+
+	prefixes.pop(str(guild.id))
+
+	with open ('prefix.json', 'w') as f:
+		json.dump(prefixes, f, indent=4)
+
+@client.command(name="help")
+async def help(ctx: commands.Context):
+	embed=discord.Embed(title="Help", description="All commands", color=0xffffff)
+	embed.set_author(name="1rom11")
+	embed.add_field(name="Code", value="Shows github code for UDBSH", inline=False)
+	embed.add_field(name="Invite", value="Sends invite link for UDBSH", inline=False)
+	embed.add_field(name="Echo", value="sends what ever in (arg) back", inline=False)
+	embed.add_field(name="Rnt", value="In a random time sends a message", inline=False)
+	await ctx.send(embed=embed)
+
+@client.command(name="invite")
+async def invite(ctx: commands.Context):
+	await ctx.send(f"`The invite link for {client.user} is` ||https://bit.ly/UDBSH-invite||")
 @client.command(name="ping")
 async def ping(ctx: commands.Context):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
 @client.command(name="echo")
 async def echo(ctx, *, arg):
-	await ctx.send(arg)
+	await ctx.send(f"`{arg}`")
+
+@client.command(name="trello")
+async def trello(ctx):
+	await ctx.send(f"`Trello page = https://trello.com/b/IiUj4tjb/udbsh`")
 
 @client.command()
-async def embed(ctx):
-    embed=discord.Embed(title="Sample Embed", url="https://github.com/1rom11/-udbsh.discord.py", description="Code fot UDBSH", color=discord.Color.white())
-    await ctx.send(embed=embed)
-
-
+async def rnt(ctx):    
+    async with ctx.typing():
+	
+        type_time = random.uniform(0.5, 2)
+        await asyncio.sleep(type_time)
+    
+    await ctx.send('`Type time = {} seconds`'.format(round(type_time, 2)))
 async def kick(ctx, member : discord.Member):
     try:
         await member.kick(reason=None)
         await ctx.send(f"{client.user} has kicked "+member.mention)
     except:
-        await ctx.send(f"{client.user} does not have the kick members permission! or user role is higher than {client.user}")
+        await ctx.send(f"{client.user} does not have the kick members permission! or user role is higher than {client.user} or {ctx.author}")
 	#token setup
 token = os.environ['token']
 os.environ.get("token")
 client.run(token)
 
 #https://discordpy.readthedocs.io/en/master/
+#https://cog-creators.github.io/discord-embed-sandbox/
